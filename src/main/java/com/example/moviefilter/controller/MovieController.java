37ce4097dc297e1de5e914937ada32dd9dc8d389
Adeweh <dorcasabang@gmail.com>
2023-05-22 15:07:50 +0100

    Refactoring

--- src/main/java/com/example/moviefilter/controller/MovieController.java
+++ src/main/java/com/example/moviefilter/controller/MovieController.java
@@ -5,10 +5,12 @@ import com.example.moviefilter.service.MovieService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movie")
@@ -18,8 +20,11 @@ public class MovieController {
         @GetMapping("/ok")
         public ResponseEntity<List<Movie>> getAllMovies() {
             return ResponseEntity.ok(movieService.getAllMovies());
-
         }
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getAMovie(@PathVariable String id) {
+        return ResponseEntity.ok(movieService.getAMovies(id));
+    }
 
 
 
