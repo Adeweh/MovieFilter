51e3e70e108496e19c39c561f7546a642ee2e7df
Adeweh <dorcasabang@gmail.com>
2023-05-22 12:46:33 +0100

    Refactoring

--- src/main/java/com/example/moviefilter/model/Movie.java
+++ src/main/java/com/example/moviefilter/model/Movie.java
@@ -6,17 +6,24 @@ import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Getter
 @Setter
 @AllArgsConstructor
-@Document
+@Document(collection = "movies")
 public class Movie {
     @Id
     private String id;
 
-    private MovieResponseData movieResponse;
-//    private String title;
-//    private int releaseYear;
-    private Genre genre;
-//    private List<Cast> actors;
+    private String imbId;
+
+    private String title;
+    private String releaseDate;
+    private String trailerLink;
+    private String poster;
+    private List<String> genres;
+    private List<String> backdrops;
+
+
 }
